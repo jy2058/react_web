a649ef52cdabf02ec7deaf3f8a1e312a6f70c0ad
kjy <jydev763@naver.com>
2020-05-31 23:39:33 +0900

    create customer db node database connection settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "다음",
-      birthday: "123333",
-      gender: "여자",
-      job: "직장인",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "네이버",
-      birthday: "122824",
-      gender: "여자",
-      job: "프로그래머",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "카카오",
-      birthday: "212321",
-      gender: "여자",
-      job: "학생",
-    },
-  ]);
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
 });
+//connection.connect();
+
+app.get("/api/customers", (req, res) => {
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
+});
+
+// app.get("/api/login", (req, res) => {
+//   res.send();
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
