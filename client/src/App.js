5d34d17e1915b13d2a03dbcccd4fafabe2c76cc9
kjy <jydev763@naver.com>
2020-05-31 21:09:31 +0900

    Add Get Customers REST API

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,25 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "다음",
-    birthday: "123333",
-    gender: "여자",
-    job: "직장인",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "네이버",
-    birthday: "122824",
-    gender: "여자",
-    job: "프로그래머",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "카카오",
-    birthday: "212321",
-    gender: "여자",
-    job: "학생",
-  },
-];
-
 class App extends Component {
+  state = {
+    customers: "",
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .then((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    console.log(response);
+    console.log(body);
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -65,19 +56,21 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map((c) => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );
-              })}
+              {this.state.customers
+                ? this.state.customers.map((c) => {
+                    return (
+                      <Customer
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                      />
+                    );
+                  })
+                : ""}
             </TableBody>
           </Table>
         </Paper>
