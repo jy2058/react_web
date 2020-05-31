268339463a366d80037920d63907b369730c9d2a
kjy <jydev763@naver.com>
2020-06-01 01:11:08 +0900

    add customer delete module

--- server.js
+++ server.js
@@ -24,9 +24,12 @@ const multer = require("multer");
 const upload = multer({ dest: "./upload" });
 
 app.get("/api/customers", (req, res) => {
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
-    res.send(rows);
-  });
+  connection.query(
+    "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 // app.get("/api/login", (req, res) => {
@@ -35,7 +38,7 @@ app.get("/api/customers", (req, res) => {
 
 app.use("/image", express.static("./upload"));
 app.post("/api/customers", upload.single("image"), (req, res) => {
-  let sql = "INSERT INTO CUSTOMERS VALUES (null, ?, ?, ?, ?, ?)";
+  let sql = "INSERT INTO CUSTOMERS VALUES (null, ?, ?, ?, ?, ?, now(), 0)";
   let image = "/image/" + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -47,4 +50,11 @@ app.post("/api/customers", upload.single("image"), (req, res) => {
   });
 });
 
+app.delete("/api/customer/:id", (req, res) => {
+  let sql = "UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?";
+  let params = [req.params.id];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+  });
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
